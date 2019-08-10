0547c3a3be2cb37dc5b2b0094e60739ede09f25a
Bugazelle <zi_cheng@qq.com>
2019-08-11 03:10:03 +0800

    [Fix] Remove the super

--- src/ExportCsvToInflux/__init__.py
+++ src/ExportCsvToInflux/__init__.py
@@ -3,15 +3,3 @@ from .csv_object import CSVObject
 from .exporter_object import ExporterObject
 from .base_object import BaseObject
 from .__version__ import __version__
-
-_version_ = __version__
-
-
-class ExportCsvToInflux(InfluxObject,
-                        CSVObject,
-                        ExporterObject,
-                        BaseObject,):
-    """ExportCsvToInflux is library to export csv data into influx db"""
-
-    def __init__(self):
-        super(ExportCsvToInflux, self).__init__()
