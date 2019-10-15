3b895b95d5d17fb5f3805f3ea4cc5e2cd540de8d
Bugazelle <zi_cheng@qq.com>
2019-10-16 01:55:05 +0800

    [Fix] Fix issue-17 & fix total count & logic enhancements

--- src/ExportCsvToInflux/base_object.py
+++ src/ExportCsvToInflux/base_object.py
@@ -1,8 +1,42 @@
+import sys
+
 class BaseObject(object):
     """BaseObject"""
 
     def __init__(self):
         self.strip_chars = ' \r\n\t/"\',\\'
+
+    @staticmethod
+    def convert_boole(target):
+        target = str(target).lower()
+        if target != 'true' and target != 'false':
+            print('Error: The expected input for {0} should be: True or False'.format(target))
+            exit(1)
+        if target == 'true':
+            target = True
+        else:
+            target = False
+
+        return target
+
+    def validate_str(self, target, ignore_exception=False):
+        """Function: validate_string
+
+        :param target: the string
+        :param ignore_exception: the True or False
+        """
+
+        get_type = type(target)
+        ignore_exception = self.convert_boole(ignore_exception)
+        try:
+            string_type = get_type is str or get_type is unicode
+        except NameError:
+            string_type = get_type is str
+        if not string_type and ignore_exception is False:
+            print('Error: The {0} is not string type. Please check.'.format(target))
+            sys.exit(1)
+
+        return string_type
 
     def str_to_list(self, string, delimiter=',', lower=False):
         """Function: str_to_list
@@ -13,12 +47,20 @@ class BaseObject(object):
         :return
         """
 
-        string_type = type(string)
-        list_tuple_type = string_type is list or string_type is tuple
+        get_type = type(string)
+        error_message = 'Error: The string should be list or string, use comma to separate. ' \
+                        'Current is: type-{0}, {1}'.format(get_type, string)
+
+        # Process if Value Error
         try:
-            str_unicode_type = string_type is str or string_type is unicode
-        except NameError:
-            str_unicode_type = string_type is str
+            bool(string)
+        except ValueError:
+            print(error_message)
+            sys.exit(1)
+
+        # Process the type
+        list_tuple_type = get_type is list or get_type is tuple
+        str_unicode_type = self.validate_str(string, True)
         if list_tuple_type:
             if lower:
                 li = [str(item).strip(self.strip_chars).lower() for item in string]
@@ -30,9 +72,9 @@ class BaseObject(object):
                 li = [item.strip(self.strip_chars).lower() for item in li]
             else:
                 li = [item.strip(self.strip_chars) for item in li]
-        elif bool(string) is False:
+        elif not string:
             li = list()
         else:
-            raise Exception('Error: The string should be list or string, use comma to separate. '
-                            'Current is: type-{0}, {1}'.format(string_type, string))
+            print(error_message)
+            sys.exit(1)
         return li
